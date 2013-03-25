a23cab91d4157d0b895c0378361102297d674785
Austin Krause <ajkrause@asu.edu>
2013-03-25 16:57:59 -0700

    Actual high score update with reponse

--- updateHighScore.php
+++ updateHighScore.php
@@ -8,9 +8,19 @@
 		  or die("Unable to connect to MySQL");
       
       @mysql_select_db("gameadm_usertest") or die( "Unable to select database");
-      
-   $query = "UPDATE userdata SET HSCORE=" . $_GET["scr"] . " WHERE ID=" . $_GET["usr"];
+      $newscore = round($_GET["scr"] / 6);
+  $query = "SELECT HSCORE FROM userdata WHERE ID=" . $_GET["usr"];
+  $response = mysql_query($query) or die(mysql_error());
+  $row = mysql_fetch_array($response);
+  $score = $row["HSCORE"];
+  
+  if($score < $newscore)
+  {
+    $score = $newscore; 
+   $query = "UPDATE userdata SET HSCORE=" . $score . " WHERE ID=" . $_GET["usr"];
    mysql_query($query) or die(mysql_error());
-   
+  }
+  
    mysql_close($dbhandle);
+   echo $score;
 ?>
